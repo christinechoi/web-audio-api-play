f59d6be3ee2b975e93d1850841a550801798d493
christinechoi <smi77y@gmail.com>
2018-05-19 22:54:16 -0400

    Create index.html and app.js files

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
 var audioContext = new AudioContext()
 
-play(0, 3, 0.5)
-play(1, 10, 0.5)
-play(2, 15, 0.5)
+startButton.onclick = play(0, 3, 0.5);
 
 function play (delay, pitch, duration) {
   var startTime = audioContext.currentTime + delay
